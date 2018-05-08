7ac7acca28d74b58bbb784706fee3c5a70a0c0b5
Jose <joseguerreroroman@yahoo.com.mx>
2018-05-08 09:55:01 -0500

    Cambio en rutas
    Se agrega la ruta Bootcamp para que pueda mostrarse en el menú de inicio

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import App from './App';
 import Formulario from './componentes/Formulario';
 import Page404 from './componentes/Page404';
 import Login from './componentes/Login';
+import Bootcamp from './componentes/Bootcamp';
 
 
 const AppRouter = () =>
@@ -15,6 +16,7 @@ const AppRouter = () =>
             <Route exact path="/" component={ Login } />
             <Route exact path="/Formulario" component={ Formulario } />
             <Route exact path="/Login" component={ Login } />
+            <Route exact path="/Bootcamp" component={ Bootcamp } />
             <Route component={ Page404 } />
         </Switch>
     </App>;
